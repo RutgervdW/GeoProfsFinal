93325758f59f61455a06a766c04d3de8341d20e3
RutgervdW <74962188+RutgervdW@users.noreply.github.com>
2022-09-26 10:37:27 +0200

    kleine aanpassing context

--- Data/GeoprofsContext.cs
+++ Data/GeoprofsContext.cs
@@ -13,7 +13,13 @@ namespace Geoprofs.Data
         public DbSet<AfwezigheidCategorie> AfwezigheidCategories { get; set; }
         public DbSet<Medewerker> Medewerkers { get; set; }
         public DbSet<Positie> Posities { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Afwezigheid>().ToTable("Afwezigheid");
+            modelBuilder.Entity<AfwezigheidCategorie>().ToTable("AfwezigheidCategorie");
+            modelBuilder.Entity<Medewerker>().ToTable("Medewerker");
+            modelBuilder.Entity<Positie>().ToTable("Positie");
+        }
     }
-
-
+        
 }
